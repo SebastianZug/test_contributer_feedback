dcdbea5adbca7ec293d78f6d0a7bf6fc03e96268
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2021-04-22 08:51:49 +0200

    Update extractGitHubData.py

--- .github/workflows/extractGitHubData.py
+++ .github/workflows/extractGitHubData.py
@@ -1,4 +1,5 @@
 from github2pandas.utility import Utility
+from github2pandas.version import Version
 from pathlib import Path
 import os
 
@@ -9,4 +10,8 @@ git_repo_owner = os.environ['CI_REPOSITORY_OWNER']
 default_data_folder = Path("data", git_repo_name)
 
 repo = Utility.get_repo(git_repo_owner, git_repo_name, github_token, default_data_folder)
+
+Version.clone_repository(repo=repo, data_root_dir=default_data_folder, github_token=github_token)
+Version.generate_version_pandas_tables(data_root_dir=default_data_folder)
+
 print("Aus die Maus!")
