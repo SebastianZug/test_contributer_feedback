a157ac6a1107782c7cf784fa5e32bfa4e41329c3
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2021-04-22 11:11:21 +0200

    Update extractGitHubData.py

--- .github/workflows/extractGitHubData.py
+++ .github/workflows/extractGitHubData.py
@@ -9,9 +9,21 @@ git_repo_owner = os.environ['CI_REPOSITORY_OWNER']
     
 default_data_folder = Path("data", git_repo_name)
 
-repo = Utility.get_repo(git_repo_owner, git_repo_name, github_token, default_data_folder)
+if __name__ == "__main__":
+    repo = Utility.get_repo(git_repo_owner, git_repo_name, github_token, default_data_folder)
 
-Version.clone_repository(repo=repo, data_root_dir=default_data_folder, github_token=github_token)
-Version.generate_version_pandas_tables(data_root_dir=default_data_folder)
+    Version.clone_repository(repo=repo, data_root_dir=default_data_folder, github_token=github_token)
+    Version.generate_version_pandas_tables(data_root_dir=default_data_folder)
+    
+    users = Utility.get_users(default_data_folder)
+    pdCommits = Version.get_version(data_root_dir=default_data_folder)
 
-print("Aus die Maus!")
+    if not users.empty:
+        users_count = users.shape[0]
+        print("%d Users" % users_count)
+
+    for index, row in users.iterrows():
+        print("%d: " % index + row["anonym_uuid"])
+
+    span = pdCommits.commited_at.max() - pdCommits.commited_at.min()
+    print(f"Project active for {span.days} days")
