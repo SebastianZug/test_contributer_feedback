2983ba47e3444746fe7cad9ec78db37b92f2ae1c
Sebastian Zug <Sebastian.Zug@informatik.tu-freiberg.de>
2021-04-23 15:14:38 +0000

    Check syntax

--- .github/workflows/extractGitHubData.py
+++ .github/workflows/extractGitHubData.py
@@ -65,7 +65,9 @@ if __name__ == "__main__":
     ax.legend(users.anonym_uuid.values)
     ax.set_xlabel("Date")
     ax.set_ylabel('Added Lines of Code')
-    print(pdFileEdits.commited_at.min())
+    print(pdFileEdits.commited_at.min().date)
+    print(pdFileEdits.commited_at.max()+ pd.Timedelta("1 days"))
+    print((pdFileEdits.commited_at.max()+ pd.Timedelta("1 days")).date)
     ax.set_xlim(pdFileEdits.commited_at.min().date, 
                 (pdFileEdits.commited_at.max()+ pd.Timedelta("1 days")).date)
     plt.tight_layout()
